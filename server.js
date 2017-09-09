c5a401915f0bf319254c555e845a7d8ae8ae1ede
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-09 08:57:18 +0700

    send data from server

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const io = require('socket.io')(3000);
+const io = require('socket.io')(4200);
 
 io.on('connection', socket => {
-    console.log(socket.id);
+    setInterval(() => {
+        const num = Math.random();
+        socket.emit('SERVER_SEND_MESSAGE', num);
+    }, 3000);
 });
+
+// arr.forEach(e => console.log(e));
+// tren server -> co nhieu tuy chon gui di
+// io Quan ly tat ca cac socket
+// socket quan ly ket noi toi 1 client
+// socket.emit('tag', value) 
+//socket.on('tag', value => console.log(value));
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,10 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,5 +14,9 @@ class App extends Component {
     );
   }
 }
+
+const socket = io('http://localhost:4200');
+
+socket.on('SERVER_SEND_MESSAGE', num => console.log(num));
 
 export default App;
