2578aa80b614ebcd676deb6b55828f034ad914fa
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-09 09:36:51 +0700

    messages in a list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
 
+const socket = io('http://localhost:4200');
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.sendMessage = this.sendMessage.bind(this);
+    this.state = {
+      messages: []
+    }
+  }
+
+  componentDidMount() {
+    socket.on('SERVER_SEND_MESSAGE', message => {
+      this.setState(prevState => ({ messages: prevState.messages.concat(message) }))
+    });
+  }
+
   sendMessage() {
     const { value } = this.refs.txtMessage;
     socket.emit('CLIENT_SEND_MESSAGE', value);
@@ -9,18 +25,16 @@ class App extends Component {
   }
 
   render() {
+    const { messages } = this.state;
     return (
       <div className="App">
         <input type="text" placeholder="Enter your message" ref="txtMessage"/>
         <br /><br />
-        <button onClick={this.sendMessage.bind(this)}>Send Message</button>
+        <button onClick={this.sendMessage}>Send Message</button>
+        { messages.map((message, index) => <p key={index}>{message}</p>) }
       </div>
     );
   }
 }
-
-const socket = io('http://localhost:4200');
-
-socket.on('SERVER_SEND_MESSAGE', num => console.log(num));
 
 export default App;
